9660495d95bb13523e7fbd7aeccbe7f3a82283dd
viandrada <victoria.andrada@gmail.com>
2014-10-17 18:40:42 -0200

    Subiendo m

---
+++ Proyecto-Core/ejbModule/m.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: m
+ *
+ */
+@Entity
+
+public class m implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "persona_sequence")
+	private Long id;
+	public m() {
+		super();
+	}
+   
+}
